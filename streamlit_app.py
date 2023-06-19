45184cff6ddb03fe0b9ea4731152d913feb53fa8
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2023-06-19 16:26:11 +0530

    Update streamlit_app.py
    added request to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,28 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+# new section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
