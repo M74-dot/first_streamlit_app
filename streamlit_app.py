72dfa205bc66e98ec8bb3b9004f0897c8cd71497
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2023-06-20 12:03:21 +0530

    Update streamlit_app.py
    added text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,12 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
+
+fruit_choice = streamlit.text_input('What fruit would you?','Banana')
+streamlit.write('The user entered ', fruit_choice)
+
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
 
 
 
